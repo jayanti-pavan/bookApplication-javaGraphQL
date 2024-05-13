40e9874d530ffcb86908531cd24473693d65efde
Pavan Kumar Jayanti <jayanti.pavan@procore.com>
2024-05-13 19:40:28 +0530

    Initial Commit from Spring Initializr

---
+++ src/main/java/com/graphqljava/tutorial/bookDetails/JavaGraphQlDemoBookDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphQlDemoBookDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGraphQlDemoBookDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphqljava/tutorial/bookDetails/JavaGraphQlDemoBookDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphQlDemoBookDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
