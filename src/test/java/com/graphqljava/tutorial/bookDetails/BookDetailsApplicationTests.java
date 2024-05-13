d53fa0d170a0daa34dcd2defab230734ea0ed363
Pavan Kumar Jayanti <jayanti.pavan@procore.com>
2024-05-13 19:51:16 +0530

    Refactored: renamed application

--- src/test/java/com/graphqljava/tutorial/bookDetails/BookDetailsApplicationTests.java
+++ src/test/java/com/graphqljava/tutorial/bookDetails/BookDetailsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaGraphQlDemoBookDetailsApplicationTests {
+class BookDetailsApplicationTests {
 
 	@Test
 	void contextLoads() {
